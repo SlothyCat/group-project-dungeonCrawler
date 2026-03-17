cab34a5406d4bec7568a1fa0fad74846dc4f8f18
SlothyCat <108041417+SlothyCat@users.noreply.github.com>
2026-03-17 22:49:21 +0800

    Modify EntityFactoryTests

--- dungeonCrawlerTests/ECS/EntityFactoryTests.swift
+++ dungeonCrawlerTests/ECS/EntityFactoryTests.swift
@@ -23,6 +23,8 @@ final class EntityFactoryTests: XCTestCase {
         super.tearDown()
     }
 
+    // MARK: - makePlayer
+
     func testMakePlayerEntityIsAlive() {
         let entity = EntityFactory.makePlayer(in: world, at: SIMD2<Float>(0, 0))
         // Entity is alive if we can query a component from it
@@ -90,113 +92,141 @@ final class EntityFactoryTests: XCTestCase {
         let entity1 = EntityFactory.makePlayer(in: world, at: SIMD2<Float>(0, 0))
         let entity2 = EntityFactory.makePlayer(in: world, at: SIMD2<Float>(10, 10))
         XCTAssertNotEqual(entity1, entity2)
-        // MARK: - makeEnemy: entity registration
+    }
 
-        func testMakeEnemyIsAlive() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertTrue(world.isAlive(entity: enemy))
+    // MARK: - makeEnemy: entity registration
+
+    func testMakeEnemyIsAlive() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertTrue(world.isAlive(entity: enemy))
+    }
+
+    func testMakeEnemyReturnsUniqueEntities() {
+        let enemy1 = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        let enemy2 = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNotEqual(enemy1, enemy2)
+    }
+
+    // MARK: - makeEnemy: TransformComponent
+
+    func testMakeEnemyPositionIsSet() {
+        let position = SIMD2<Float>(100, 200)
+        let enemy = EntityFactory.makeEnemy(in: world, at: position, type: .charger)
+        let transform = world.getComponent(type: TransformComponent.self, for: enemy)
+        XCTAssertNotNil(transform)
+        XCTAssertEqual(transform!.position.x, 100, accuracy: 0.001)
+        XCTAssertEqual(transform!.position.y, 200, accuracy: 0.001)
+    }
+
+    func testMakeEnemyRotationIsZero() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        let transform = world.getComponent(type: TransformComponent.self, for: enemy)
+        XCTAssertNotNil(transform)
+        XCTAssertEqual(transform!.rotation, 0, accuracy: 0.001)
+    }
+
+    func testMakeEnemyDefaultBaseScaleUsesTypeScale() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        let transform = world.getComponent(type: TransformComponent.self, for: enemy)
+        XCTAssertNotNil(transform)
+        XCTAssertEqual(transform!.scale, EnemyType.charger.scale, accuracy: 0.001)
+    }
+
+    func testMakeEnemyScaleIsBaseScaleTimesTypeScale() {
+        let baseScale: Float = 2.0
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .tower, baseScale: baseScale)
+        let transform = world.getComponent(type: TransformComponent.self, for: enemy)
+        XCTAssertNotNil(transform)
+        XCTAssertEqual(transform!.scale, baseScale * EnemyType.tower.scale, accuracy: 0.001)
+    }
+
+    // MARK: - makeEnemy: SpriteComponent
+
+    func testMakeEnemyHasSpriteComponent() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNotNil(world.getComponent(type: SpriteComponent.self, for: enemy))
+    }
+
+    func testMakeEnemyTextureMatchesType() {
+        for enemyType in [EnemyType.charger, .mummy, .ranger, .tower] {
+            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: enemyType)
+            let sprite = world.getComponent(type: SpriteComponent.self, for: enemy)
+            XCTAssertNotNil(sprite, "Missing sprite for \(enemyType)")
+            XCTAssertEqual(sprite!.textureName, enemyType.textureName, "Texture mismatch for \(enemyType)")
         }
+    }
 
-        func testMakeEnemyReturnsUniqueEntities() {
-            let enemy1 = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            let enemy2 = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertNotEqual(enemy1, enemy2)
-        }
+    // MARK: - makeEnemy: EnemyTagComponent
 
-        // MARK: - makeEnemy: TransformComponent
+    func testMakeEnemyHasEnemyTag() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNotNil(world.getComponent(type: EnemyTagComponent.self, for: enemy))
+    }
 
-        func testMakeEnemyPositionIsSet() {
-            let position = SIMD2<Float>(100, 200)
-            let enemy = EntityFactory.makeEnemy(in: world, at: position, type: .charger)
-            let transform = world.getComponent(type: TransformComponent.self, for: enemy)
-            XCTAssertEqual(transform?.position.x, 100)
-            XCTAssertEqual(transform?.position.y, 200)
-        }
+    func testMakeEnemyTagMatchesType() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .mummy)
+        let tag = world.getComponent(type: EnemyTagComponent.self, for: enemy)
+        XCTAssertNotNil(tag)
+        XCTAssertTrue(tag!.enemyType == .mummy)
+    }
 
-        func testMakeEnemyDefaultScale() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            let transform = world.getComponent(type: TransformComponent.self, for: enemy)
-            XCTAssertEqual(transform?.scale, 1)
-        }
+    // MARK: - makeEnemy: VelocityComponent and EnemyStateComponent
 
-        func testMakeEnemyCustomScale() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger, scale: 2.5)
-            let transform = world.getComponent(type: TransformComponent.self, for: enemy)
-            XCTAssertEqual(transform?.scale, 2.5)
-        }
+    func testMakeEnemyHasVelocityComponent() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNotNil(world.getComponent(type: VelocityComponent.self, for: enemy))
+    }
 
-        func testMakeEnemyRotationIsZero() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            let transform = world.getComponent(type: TransformComponent.self, for: enemy)
-            XCTAssertEqual(transform?.rotation, 0)
-        }
+    func testMakeEnemyVelocityStartsAtZero() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        let velocity = world.getComponent(type: VelocityComponent.self, for: enemy)
+        XCTAssertNotNil(velocity)
+        XCTAssertEqual(velocity!.linear.x, 0, accuracy: 0.001)
+        XCTAssertEqual(velocity!.linear.y, 0, accuracy: 0.001)
+    }
 
-        // MARK: - makeEnemy: SpriteComponent
+    func testMakeEnemyHasEnemyStateComponent() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNotNil(world.getComponent(type: EnemyStateComponent.self, for: enemy))
+    }
 
-        func testMakeEnemyHasSpriteComponent() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertNotNil(world.getComponent(type: SpriteComponent.self, for: enemy))
-        }
+    func testMakeEnemyStartsInWanderMode() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        let state = world.getComponent(type: EnemyStateComponent.self, for: enemy)
+        XCTAssertNotNil(state)
+        XCTAssertTrue(state!.mode == .wander)
+    }
 
-        func testMakeEnemyTextureMatchesType() {
-            for type in [EnemyType.charger, .mummy, .ranger, .tower] {
-                let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: type)
-                let sprite = world.getComponent(type: SpriteComponent.self, for: enemy)
-                XCTAssertEqual(sprite?.textureName, type.textureName, "Texture mismatch for \(type)")
-            }
-        }
+    // MARK: - makeEnemy: no player components
 
-        // MARK: - makeEnemy: EnemyTagComponent
+    func testMakeEnemyHasNoInputComponent() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNil(world.getComponent(type: InputComponent.self, for: enemy))
+    }
 
-        func testMakeEnemyHasEnemyTag() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertNotNil(world.getComponent(type: EnemyTagComponent.self, for: enemy))
-        }
+    func testMakeEnemyHasNoPlayerTag() {
+        let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
+        XCTAssertNil(world.getComponent(type: PlayerTagComponent.self, for: enemy))
+    }
 
-        func testMakeEnemyTagMatchesType() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .mummy)
-            let tag = world.getComponent(type: EnemyTagComponent.self, for: enemy)
-            XCTAssertEqual(tag?.enemyType, .mummy)
-        }
+    // MARK: - makeEnemy: world queries
 
-        // MARK: - makeEnemy: no player components
+    func testEnemiesQueryableByTag() {
+        EntityFactory.makeEnemy(in: world, at: SIMD2(0, 0),   type: .charger)
+        EntityFactory.makeEnemy(in: world, at: SIMD2(100, 0), type: .mummy)
+        let enemies = world.entities(with: EnemyTagComponent.self)
+        XCTAssertEqual(enemies.count, 2)
+    }
 
-        func testMakeEnemyHasNoInputComponent() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertNil(world.getComponent(type: InputComponent.self, for: enemy))
-        }
+    func testPlayerAndEnemiesAreIsolated() {
+        EntityFactory.makePlayer(in: world, at: .zero)
+        EntityFactory.makeEnemy(in: world, at: SIMD2(100, 0), type: .charger)
 
-        // For now the enemy is stationary
-        // TODO: REMOVE AFTER ENEMY HAS BEEN GRANTED FUNCTIONALITY TO MOVE
-        func testMakeEnemyHasNoVelocityComponent() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertNil(world.getComponent(type: VelocityComponent.self, for: enemy))
-        }
+        let players = world.entities(with: PlayerTagComponent.self)
+        let enemies = world.entities(with: EnemyTagComponent.self)
 
-        func testMakeEnemyHasNoPlayerTag() {
-            let enemy = EntityFactory.makeEnemy(in: world, at: .zero, type: .charger)
-            XCTAssertNil(world.getComponent(type: PlayerTagComponent.self, for: enemy))
-        }
-
-        // MARK: - makeEnemy: world queries
-
-        func testEnemiesQueryableByTag() {
-            EntityFactory.makeEnemy(in: world, at: SIMD2(0, 0),   type: .charger)
-            EntityFactory.makeEnemy(in: world, at: SIMD2(100, 0), type: .mummy)
-            let enemies = world.entities(with: EnemyTagComponent.self)
-            XCTAssertEqual(enemies.count, 2)
-        }
-
-        func testPlayerAndEnemiesAreIsolated() {
-            EntityFactory.makePlayer(in: world, at: .zero)
-            EntityFactory.makeEnemy(in: world, at: SIMD2(100, 0), type: .charger)
-
-            let players = world.entities(with: PlayerTagComponent.self)
-            let enemies = world.entities(with: EnemyTagComponent.self)
-
-            XCTAssertEqual(players.count, 1)
-            XCTAssertEqual(enemies.count, 1)
-            XCTAssertNotEqual(players.first, enemies.first)
-        }
+        XCTAssertEqual(players.count, 1)
+        XCTAssertEqual(enemies.count, 1)
+        XCTAssertNotEqual(players.first, enemies.first)
     }
 }
