4f09fc44315a0b7049d385f126ba516fbefa4908
YuLetian <yu.letian66@gmail.com>
2026-03-17 17:16:28 +0800

    Follow owner

---
+++ dungeonCrawler/ECS/Systems/WeaponSystem.swift
@@ -0,0 +1,27 @@
+import Foundation
+import simd
+
+class WeaponSystem: System {
+    var priority: Int = 50
+    
+    private var gameTime: Float
+
+    init() {
+        self.gameTime = 0
+    }
+
+    func update(deltaTime: TimeInterval, world: World) {
+        self.gameTime += deltaTime
+
+        for (weaponEntity, weaponComponent, ownerComponent) in world.entities(with: WeaponComponent.self, and: OwnerComponent.self) {
+            let ownerEntity = ownerComponent.ownerEntity
+            if let ownerTransformComponent = world.getComponent(type: TransformComponent.self, for: ownerEntity) {
+                world.modifyComponent(type: TransformComponent.self, for: weaponEntity) { transform in
+                    transform.position = ownerTransformComponent.position + ownerComponent.offset
+                }
+            }
+        }
+
+
+    }
+}
