9ba2d7e793176f7732d195cdc96d823960a52c1a
YuLetian <yu.letian66@gmail.com>
2026-03-21 14:28:28 +0800

    Refactor: safeguard EffectiveRangeComponent exist

--- dungeonCrawler/ECS/Systems/ProjectileSystem.swift
+++ dungeonCrawler/ECS/Systems/ProjectileSystem.swift
@@ -12,14 +12,21 @@ public final class ProjectileSystem: System {
     public let priority: Int = 60 // After weapon spawn new projectiles
     public func update(deltaTime: Double, world: World) {
         let dt = Float(deltaTime)
-        for (projectileEntity, _, velocityComponent, _) in world.entities(with: ProjectileComponent.self, and: VelocityComponent.self, and: TransformComponent.self) {
+        for (projectileEntity, _, velocityComponent, _, _) in world.entities(
+            with: ProjectileComponent.self,
+            and: VelocityComponent.self,
+            and: TransformComponent.self,
+            and: EffectiveRangeComponent.self) {
             world.modifyComponent(type: TransformComponent.self, for: projectileEntity) { transform in
                 transform.position += velocityComponent.linear * dt
             }
+            let distanceTraveled = simd_length(velocityComponent.linear) * dt
+            var remainingRange: Float = .greatestFiniteMagnitude
             world.modifyComponent(type: EffectiveRangeComponent.self, for: projectileEntity) { rangeComponent in
-                rangeComponent.value.current -= simd_length(velocityComponent.linear) * dt
+                rangeComponent.value.current -= distanceTraveled
+                remainingRange = rangeComponent.value.current
             }
-            if let range = world.getComponent(type: EffectiveRangeComponent.self, for: projectileEntity), range.value.current <= 0 {
+            if remainingRange <= 0 {
                 world.destroyEntity(entity: projectileEntity)
             }
         }
