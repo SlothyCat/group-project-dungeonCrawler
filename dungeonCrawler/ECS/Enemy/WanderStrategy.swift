2dc4aa0483d2ffe8ab8edf087bbff97f892df29e
SlothyCat <108041417+SlothyCat@users.noreply.github.com>
2026-03-27 21:49:50 +0800

    Create EnemyAIStrategy and WanderStrategy

---
+++ dungeonCrawler/ECS/Enemy/WanderStrategy.swift
@@ -0,0 +1,41 @@
+//
+//  WanderStrategy.swift
+//  dungeonCrawler
+//
+//  Created by Wen Kang Yap on 27/3/26.
+//
+
+import Foundation
+import simd
+
+/// A strategy that Enemy will wander around by choosing a random point within the wanderRadius
+/// Simple AI movement when Player is not in range
+public final class WanderStrategy: EnemyAIStrategy {
+
+    public func update(entity: Entity, transform: TransformComponent, playerPos: SIMD2<Float>, world: World) {
+        /// System that will call this strategy will ensure that Entity has EnemyStateComponent but extra guard here
+        guard let currentState = world.getComponent(type: EnemyStateComponent.self, for: entity) else { return }
+
+        world.modifyComponent(type: EnemyStateComponent.self, for: entity) { s in
+            let arrivalThreshold: Float = 8
+
+            if s.wanderTarget == nil ||
+                simd_length(transform.position - s.wanderTarget!) < arrivalThreshold {
+                let angle = Float.random(in: 0..<(2 * .pi))
+                let radius = Float.random(in: 0...s.wanderRadius)
+                s.wanderTarget = transform.position +
+                SIMD2(cos(angle) * radius, sin(angle) * radius)
+            }
+        }
+
+        // extra verification that target has been set before calculating velocity to target
+        guard let target = world.getComponent(type: EnemyStateComponent.self, for: entity)?.wanderTarget else { return }
+
+        let wanderDelta = target - transform.position
+        guard simd_length_squared(wanderDelta) > 1e-6 else { return }
+
+        world.modifyComponent(type: VelocityComponent.self, for: entity) { vel in
+            vel.linear = normalize(wanderDelta) * currentState.wanderSpeed
+        }
+    }
+}
